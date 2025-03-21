61aaf1c029883b1d50929d10413ee965cdb4d2fb
ZundamonnoVRChatkaisetu <takot6656@gmail.com>
2025-03-21 17:25:16 +0900

    Add multimodal UI components index file

---
+++ src/components/multimodal/index.ts
@@ -0,0 +1,5 @@
+// Export all multimodal components for easy imports
+export * from './file-upload';
+export * from './image-viewer';
+export * from './audio-player';
+export * from './document-viewer';
